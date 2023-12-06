33e040c6316729c72e2ec08d0b6303834bd7c310
MIGU3L <99838970+miguelgustavo583@users.noreply.github.com>
2023-12-06 14:51:27 -0300

    Add files via upload

---
+++ Filtro_Auto/js/funcoes.js
@@ -0,0 +1,42 @@
+$(function(){
+    $.getJSON("js/automoveis.json", function(data) {
+        for(var i = 0; i <  data['automoveis'].length; i++){
+            $("#TableData").append("<tr>");
+            $("#TableData").append("<td scope='col'>" + data['automoveis'][i]['MARCA'] + "</td>");
+            $("#TableData").append("<td scope='col'>" + data['automoveis'][i]['CARRO'] + "</td>");
+            $("#TableData").append("<td scope='col'>" + data['automoveis'][i]['CAMBER_DIANTEIRO_MIN'] + "</td>");
+            $("#TableData").append("<td scope='col'>" + data['automoveis'][i]['CAMBER_DIANTEIRO_MAX'] + "</td>");
+            $("#TableData").append("<td scope='col'>" + data['automoveis'][i]['CASTER_DIANTEIRO_MIN'] + "</td>");
+            $("#TableData").append("<td scope='col'>" + data['automoveis'][i]['CASTER_DIANTEIRO_MAX'] + "</td>");
+            $("#TableData").append("<td scope='col'>" + data['automoveis'][i]['CONVERGENCIA_DIANTEIRA_MIN'] + "</td>");
+            $("#TableData").append("<td scope='col'>" + data['automoveis'][i]['CONVERGENCIA_DIANTEIRA_MAX'] + "</td>");
+            $("#TableData").append("<td scope='col'>" + data['automoveis'][i]['CAMBER_TRASEIRA_MIN'] + "</td>");
+            $("#TableData").append("<td scope='col'>" + data['automoveis'][i]['CAMBER_TRASEIRA_MAX'] + "</td>");
+            $("#TableData").append("<td scope='col'>" + data['automoveis'][i]['CONVERGENCIA_TRASEIRA_MIN'] + "</td>");
+            $("#TableData").append("<td scope='col'>" + data['automoveis'][i]['CONVERGENCIA_TRASEIRA_MAX'] + "</td>");
+        }
+    });
+});
+
+$(function () {
+    $("#Filtro").submit(function (event) {
+        event.preventDefault();
+        var nome = $("#nome").val();
+        var marca = $("#marca").val();
+        $("#TableData").empty();
+        $.getJSON("js/automoveis.json", function (data) {
+            data['automoveis'].forEach(function (automovel) {
+                if ((nome === '' || automovel['CARRO'].includes(nome)) &&
+                    (marca === '' || automovel['MARCA'].includes(marca))) {
+                    $("#TableData").append("<tr>");
+                    for (var prop in automovel) {
+                        $("#TableData").append("<td scope='col'>" + automovel[prop] + "</td>");
+                    }
+                    $("#TableData").append("</tr>");
+                }
+            });
+        });
+    });  
+});
+
+ 
